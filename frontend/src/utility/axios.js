0bc760187f55d548f6b27541d15e70be3ec7ad4e
Saqibaqeel <Saquibaqeel@gmail.com>
2025-06-22 23:05:08 +0530

    fixed the based url

--- frontend/src/utility/axios.js
+++ frontend/src/utility/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({ // Adjust the base URL as need
+    baseURL:'https://jobmanage-1.onrender.com/',
     withCredentials: true, 
 });
 export default axiosInstance
